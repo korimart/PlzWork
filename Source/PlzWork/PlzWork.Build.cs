e1fcf892e5e74bfa6ac3c85b06506611cc3a8f66
허윤 <korimart@n2o.studio>
2022-11-16 12:41:23 +0900

    FMOD

--- Source/PlzWork/PlzWork.Build.cs
+++ Source/PlzWork/PlzWork.Build.cs
@@ -10,7 +10,7 @@ public class PlzWork : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "FMODStudio" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
