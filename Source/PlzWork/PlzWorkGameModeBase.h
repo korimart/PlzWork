beac753ede2e298b1a47dcd10f8868c92efa843b
허윤 <korimart@n2o.studio>
2022-11-16 12:25:31 +0900

    init

---
+++ Source/PlzWork/PlzWorkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlzWorkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLZWORK_API APlzWorkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
